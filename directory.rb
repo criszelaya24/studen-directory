3381eadccb8e5b4747c474a1b3de89fe783011b8
PalaciosZ <criszelaya24@hotmail.com>
2018-10-15 18:50:31 -0600

    Printing the student with 12 or less characters

--- directory.rb
+++ directory.rb
@@ -1,37 +1,42 @@
-# Let's put all students into an array
-students = [
-	"Dr. Hannibal Lecter",
-	"Darth Vader",
-	"Nurse Ratched",
-	"Micheal Corleone",
-	"Alex DeLarge",
-	"The Wicked Witch of the West",
-	"Terminator",
-	"Freddy Krueger",
-	"The Joker",
-	"Joffrey Baratheon",
-	"Norman Bates"
-]
-
 def print_header
-	puts "The students of Villains Academy"
+	puts "The students of Villains Academy with names shorter than 12 characters are:"
 	puts "-----------------"
 end
 
 def prints(names)
-	count = 0
-	puts "Give me a letter to print the names of the students with it!"
-	letter = gets.chomp.upcase
-	print_header
-	names.each do |name|
-		if name.start_with?(letter)
-			puts name
-			count += 1
+	@count = 0
+	names.each_with_index do |name, idx|
+		if name[:name].length <= 12
+			puts "#{idx+1}. #{name[:name]} (#{name[:cohort]} cohort)"
+			@count += 1
 		end
-	end
-	print "Overall, we have #{count} great students with the letter: #{letter} \n"	
+	end	
 end
 
-# NOthing happens until we call the methods
-prints(students)
+def print_footer(names)
+	print "Overall, we have #{@count} great students \n"	
+end
 
+def input_students
+	puts "Please enter the name of students"
+	puts "To finish, just hit return twice"
+	#Create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while name != '' do
+		#Add the student hash to the array
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	# Return the array of students
+	students
+end
+# Nothing happens until we call the methods
+students = input_students
+print_header
+prints(students)
+print_footer(students)
